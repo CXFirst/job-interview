f4e72aef4faa3a88fb5a8a7eb05e200b6dd776c7
bweber <bweber@qualimetrie.com>
2022-07-06 15:01:39 +0200

    fix(app-footer): change styles

--- app/components/footer.js
+++ app/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='text-center'>
-      <h4 style={{ backgroundColor: "blue", fontSize: "large" }}>Source code based on <a href='https://github.com/drhectapus/Calculator-React' target="_blank">Github</a></h4>
+      <h4 style={{ fontSize: "small" }}>Source code based on <a href='https://github.com/drhectapus/Calculator-React' target="_blank">Github</a></h4>
     </footer>
   )
 }
